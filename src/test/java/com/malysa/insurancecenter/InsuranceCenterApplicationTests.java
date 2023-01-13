ad3ecaebbed3417b935551673fb7410722bee97e
KKMalysa <karol.malysa@interia.pl>
2023-01-13 22:31:25 +0100

    Initial commit

---
+++ src/test/java/com/malysa/insurancecenter/InsuranceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malysa.insurancecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
